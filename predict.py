5714e08218f29386d84152afe15721f74e49c43f
kimhava7 <89089011+kimhava7@users.noreply.github.com>
2023-03-17 20:58:20 +0700

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import numpy as np
 import pickle
 
 filename = 'model.pickle'
@@ -6,6 +7,7 @@ filename = 'model.pickle'
 model = pickle.load(open(filename, "rb"))
 st.title('REVENUE PREDICTION')
 tem = st.number_input('Input Temperature')
+tem = np.array(tem).reshape(-1,1)
 if st.button('Predict'):
   result = model.predict(tem)
   st.success(f'{result}')
