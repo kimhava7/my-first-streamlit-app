783e869bf292838c9786ad2e9f2fadb4d70e345f
kimhava7 <89089011+kimhava7@users.noreply.github.com>
2023-03-17 14:05:15 +0700

    Update predict.py

--- predict.py
+++ predict.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pickle
 
 filename = 'model.pickle'
-pickle.dump(model, open(filename, "wb"))
+
 model = pickle.load(open(filename, "rb"))
 st.title('REVENUE PREDICTION')
 tem = st.number_input('Input Temperature')
